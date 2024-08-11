d0de93b9fb6238f5716bb467163ab3cf38349785
0verkil <112268976+0verkil@users.noreply.github.com>
2024-08-11 11:57:50 +0200

    header animation

---
+++ src/lib/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from "svelte";
+    import { cubicIn, cubicOut } from "svelte/easing";
+    import { fly, slide } from "svelte/transition";
+
+    let domLoaded = false;
+    let animDone = false;
+
+    onMount(() => domLoaded = true)
+</script>
+
+{#if domLoaded}
+<div transition:fly={{ duration:1000, x: "-66.67vw", opacity: 1, easing: cubicOut }} class="absolute top-5 border-y-evyellow border-y-[1em] w-2/3 -z-10"></div>
+<div class="absolute top-5 left-2/3 border-t-transparent border-l-evyellow border-t-[2em] border-l-[3em] -z-10"></div>
+<div transition:fly={{ duration:1000, delay: 980, x: "-3em", opacity:1, easing: cubicOut}} class="absolute top-5 w-[3em] h-[2em] bg-black z-10 transition-transform duration-1000" style="left: calc(66.67% + 3em)"></div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script>
     import { fade, fly, scale } from "svelte/transition";
     import { afterUpdate, onMount } from "svelte";
-    import { cubicIn, cubicOut, elasticInOut } from "svelte/easing";
+    import { cubicIn, cubicInOut, cubicOut, elasticIn, elasticInOut, quintIn } from "svelte/easing";
     import ContentBox from "../lib/ContentBox.svelte";
     import { base } from "$app/paths";
     import MainPageSplash from "../lib/MainPageSplash.svelte";
+    import Header from "../lib/Header.svelte";
 
     let domLoaded = false;
     let firstUpdate = false;
@@ -26,14 +27,12 @@
 
 <div class="text-black dark:text-white">
     <!-- Header (todo) -->
-    <div class="text-center mt-[1em]"><h1 class="text-3xl">Escape Velocity</h1></div>
+    <Header></Header>
+    <div class="text-center mt-[1em]"><h1 class="text-3xl text-black font-semibold">Escape Velocity</h1></div>
     {#if domLoaded}
-       
-        <div class="text-center w-1/2 mx-auto" on:introstart={setLoaded} transition:fly={{ x: -300, duration: 1000, easing: elasticInOut }}>We are team 14343 escape velocity, a FIRST Tech Challenge team based in Bellevue, WA.
-        More yappage here idk</div>
         <div style="background-image: linear-gradient(to right, #F4C032, #FF8800);">
             <div class="relative w-1/2 mx-auto mt-10 text-black flex flex-row" >
-                <div transition:fly={{x: -300, duration: 1000, easing: cubicOut }} class="mt-[10%] text-xl z-10">
+                <div on:introstart={setLoaded} transition:fly={{x: -300, duration: 1000, easing: cubicOut }} class="mt-[10%] text-xl z-10">
                     <h1 class="text-3xl font-bold">The Future of Innovation.</h1>
                     <hr class="border-black border-t-2 transition-all delay-[400ms] duration-1000 {firstUpdate ? "w-1/3" :"w-0"} mb-4">
 <pre>
@@ -51,11 +50,12 @@ sdajuhdsaiuhsdnaodisj
             </div>
         </div>
         
+    
+        <div transition:fly={{x: 30, duration: 1000}} class="flex items-center align-middle flex-col justify-center text-center w-2/3 mx-auto mt-3">
+            <h1 class="text-3xl font-bold mb-2">We are Leaders.</h1>
+            <hr class="mb-4 border-t-2 transition-all duration-1000 delay-[1s] {!firstUpdate? "w-0" : "w-1/5"}">
+            dshauodijsai
+        </div>
     {/if}
-    <div class="flex items-center align-middle flex-col justify-center text-center w-2/3 mx-auto mt-3">
-        <h1 class="text-3xl font-bold mb-2">We are Leaders.</h1>
-        <hr class="mb-4 transition-all duration-1000 delay-[1s] {!domLoaded? "w-0" : "w-1/5"}">
-        dshauodijsai
-    </div>
     
 </div>
