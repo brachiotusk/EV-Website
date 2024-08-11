e1a90e4e471bf3a1b3a4af363f71a935fdd4d49f
0verkil <112268976+0verkil@users.noreply.github.com>
2024-08-11 12:45:22 +0200

    front page text updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,16 +31,11 @@
     {#if domLoaded}
         <div style="background-image: linear-gradient(to right, #F4C032, #FF8800);">
             <div class="relative w-1/2 mx-auto mt-10 text-black flex flex-row" >
-                <div on:introstart={setLoaded} transition:fly={{x: -300, duration: 1000, easing: cubicOut }} class="mt-[10%] text-xl z-10">
-                    <h1 class="text-3xl font-bold">The Future of Innovation.</h1>
+                <div on:introstart={setLoaded} transition:fly={{x: -300, duration: 1000, easing: cubicOut }} class="mt-[10%] text-xl z-10 max-w-60">
+                    <h1 class="text-3xl font-bold">Innovating for the Future.</h1>
                     <hr class="border-black border-t-2 transition-all delay-[400ms] duration-1000 {firstUpdate ? "w-1/3" :"w-0"} mb-4">
-<pre>
-yap yap we are so cool2
-we make cool things
-robot robot yay
-more yappage
-sdajuhdsaiuhsdnaodisj
-</pre>
+                    With a long-standing history of success, our team is always innovating, whether it is
+                    our unique Differential Swerve drive or our custom 3d-printed designs.
                 </div>
                 <div>
                     <img src="{base}/evcsrobo.png" alt="robot" class="w-auto h-[40em] -z-20"/>
@@ -50,10 +45,10 @@ sdajuhdsaiuhsdnaodisj
         </div>
         
     
-        <div transition:fly={{x: 30, duration: 1000}} class="flex items-center align-middle flex-col justify-center text-center w-2/3 mx-auto mt-3">
-            <h1 class="text-3xl font-bold mb-2">We are Leaders.</h1>
-            <hr class="mb-4 border-t-2 transition-all duration-1000 delay-[1s] {!firstUpdate? "w-0" : "w-1/5"}">
-            dshauodijsai
+        <div transition:fly={{x: 30, duration: 1000}} class="flex items-center align-middle flex-col justify-center text-center w-2/3 mx-auto mt-3 max-w-6xl">
+            <h1 class="text-2xl font-bold mb-2">Who We Are</h1>
+            <hr class="border-black dark:border-white mb-4 border-t-2 transition-all duration-1000 delay-[1s] {!firstUpdate? "w-0" : "w-1/5"}">
+            We are 14343 Escape Velocity, an 8 person FIRST® Tech Challenge team from Bellevue, Washington.
         </div>
     {/if}
     
