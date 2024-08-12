68457f547c39a6cf6d5f472615a2cadc3c7e05e9
0verkil <112268976+0verkil@users.noreply.github.com>
2024-08-12 12:34:10 +0200

    repush to fix gh pages

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -14,8 +14,8 @@
 {#if domLoaded}
 <div class="flex mt-5 justify-between">
     <div style="clip-path: polygon(0% 0%, calc(100% - 2em) 0%, 100% 100%, 0% 100%);" on:introstart={() => firstUpdate = true} transition:fly={{ duration:1000, x: "-66.67vw", opacity: 1, easing: cubicOut }} 
-        class="h-[3em] max-h-[3em] text-black bg-evyellow w-[calc(100%-3em+2px)] md:w-2/3 transition-width duration-1000">
-        <div class="text-center text-black font-semibold text-xl w-full md:hidden flex">
+        class="h-[3em] max-h-[3em] text-black bg-evyellow w-full md:w-2/3 transition-all duration-1000">
+        <div class="items-center text-center text-black font-semibold text-xl w-full h-full flex md:hidden justify-evenly">
             <h1>Escape Velocity</h1>
         </div>
         <div class="items-center text-center text-black font-semibold text-xl w-full h-full hidden md:flex justify-evenly">
@@ -32,7 +32,7 @@
 
 
     <div style="clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 2em 100%); " transition:fly={{ duration: 500, x: 300, opacity: 1, easing: cubicOut, delay: 300}} 
-        class="h-[3em] w-[5em] bg-evorange">
+        class="h-[3em] w-[5em] bg-evorange">  
         <!-- <div class="absolute top-0 -left-[2em] -z-10 border-b-transparent border-r-evorange border-b-[3em] border-r-[2em] {firstUpdate ? "" : "translate-x-2"} transition-transform duration-1000"></div> -->
         <a href="{base}/">
             <img src="{base}/evlogo.png" alt="logo" class="mt-1 ml-[calc(100%-3em)] h-[2.5em]"/>
