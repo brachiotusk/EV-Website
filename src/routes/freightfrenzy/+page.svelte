c86f471f4063ef30f8cb59f991842997c2f181a7
0verkil <112268976+0verkil@users.noreply.github.com>
2024-08-18 13:12:22 -0700

    fixed links

--- src/routes/freightfrenzy/+page.svelte
+++ src/routes/freightfrenzy/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import { base } from "$app/paths";
     import ExpandingHr from "../../lib/common/ExpandingHR.svelte";
     import PageWrapper from "../../lib/common/PageWrapper.svelte";
     import Header from "../../lib/layout/Header.svelte";
@@ -11,6 +12,6 @@
     overall game worked. For this year, we were mentored by a couple of old members of Escape Velocity, and with their help we
      worked on a simple 4 wheel drive that had a arm that would lift and pick up items from the rolling intake on the bottom and then 
      it would lock into a box to be deposited into the towers for scoring.
-     <img src="/freightfrenzy/evff.jpg" alt="The Escape Velocity Freight Frenzy robot scoring a block onto a blue hub." class="my-4 max-h-[30em]"/>
+     <img src="{base}/freightfrenzy/evff.jpg" alt="The Escape Velocity Freight Frenzy robot scoring a block onto a blue hub." class="my-4 max-h-[30em]"/>
      <i>The Escape Velocity Freight Frenzy robot scoring a block onto a blue hub.</i>
 </PageWrapper>
