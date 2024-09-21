b5da13b5a0bc39a815b68168333b63717ee3d674
0verkil <112268976+0verkil@users.noreply.github.com>
2024-09-20 20:25:54 -0700

    fix header chips

--- src/lib/common/LinkChip.svelte
+++ src/lib/common/LinkChip.svelte
@@ -3,7 +3,7 @@
     export let func;
 </script>
 
-<div style="clip-path: polygon(0% 50%, 20% 100%, 80% 100%, 100% 50%, 80% 0%, 20% 0%)" class="bg-evyellow hover:bg-evorange transition duration-500 hover:scale-125 py-2 px-4">
+<div style="clip-path: polygon(0% 50%, 1em 100%, calc(100% - 1em) 100%, 100% 50%, calc(100% - 1em) 0%, 1em 0%)" class="bg-evyellow hover:bg-evorange transition duration-500 hover:scale-125 py-2 px-4">
     {#if href}
     <a href={href}>
         <slot/>
--- src/lib/layout/Header.svelte
+++ src/lib/layout/Header.svelte
@@ -12,6 +12,7 @@
     let showNavbar = false;
     let animOver = false;
     let currentMenu = "main";
+    let p = new Date().getMonth() == 5;
 
     onMount(() => domLoaded = true)
 
@@ -29,11 +30,13 @@
         { name: "CENTERSTAGE", menus: ["seasons"], href: "/centerstage"},
         { name: "Into the Deep", menus: ["seasons"], href: "/intothedeep"}
     ];
+
+    let thingWidth = 1.5;
 </script>
 
 {#if domLoaded}
 <div class="flex mt-5 justify-between">
-    <div style="clip-path: polygon(0% 0%, calc(100% - 2em) 0%, 100% 100%, 0% 100%);" on:introstart={() => firstUpdate = true} on:introend={() => animOver = true} transition:fly={{ duration:1000, x: "-66.67vw", opacity: 1, easing: cubicOut }} 
+    <div style="clip-path: polygon(0% 0%, calc(100% - 2em) 0%, 100% 100%, 0% 100%);" on:introstart={() => firstUpdate = true} on:introend={() => animOver = true} transition:fly={{ duration:1000, x: "-66.7vw", opacity: 1, easing: cubicOut }} 
         class="h-[3em] max-h-[3em] text-black bg-evyellow w-[calc(100%-3em+2px)] md:w-2/3 transition-all duration-1000">
         <div class="items-center text-center text-black font-semibold text-xl w-full h-full flex lg:hidden justify-evenly">
             <button type="button" on:click={() => {showNavbar=!showNavbar; console.log(showNavbar)}}>
@@ -46,8 +49,11 @@
         
         <div class="items-center text-center text-black font-semibold text-xl w-full h-full hidden lg:flex justify-evenly">
             {#each headerLinks.filter((link) => link.menus.includes(currentMenu)) as link (link.name)}
-                <div animate:flip={{ duration: 1000 }} out:fly={{x: currentMenu == "main" ? "-50vw" : "50vw", opacity: 1, duration: 1000}} in:fly={{x: currentMenu == "main" ? "-50vw" : "50vw", opacity: 1, duration: 1000}}  style="clip-path: polygon(0% 50%, 20% 100%, 80% 100%, 100% 50%, 80% 0%, 20% 0%)" class="bg-evyellow hover:bg-evorange transition duration-500 hover:scale-125 py-2 px-4">
-                    {#if link.href}
+                <div animate:flip={{ duration: 1000 }} out:fly={{x: currentMenu == "main" ? "-70vw" : "70vw", opacity: 1, duration: 1000}} in:fly={{x: currentMenu == "main" ? "-70vw" : "70vw", opacity: 1, duration: 1000}}>
+                <!-- style="clip-path: polygon(0% 50%, 20% 100%, 80% 100%, 100% 50%, 80% 0%, 20% 0%)" class="bg-evyellow hover:bg-evorange transition duration-500 hover:scale-125 py-2 px-4"> -->
+                    <LinkChip href={link.href} func={link.func}>{@html link.name}</LinkChip>
+                    
+                    <!-- {#if link.href}
                     <a href={base + link.href}>
                         {@html link.name}
                     </a>
@@ -55,7 +61,7 @@
                     <button type="button" on:click={() => link.func()}>
                         {@html link.name}
                     </button>
-                    {/if}
+                    {/if} -->
                 </div>
             {/each}
                 
@@ -73,7 +79,17 @@
         </a>
     </div>
 </div>
+
 {#if showNavbar}
 <MobileNavbar func={toggleNavbar}/>
+{/if}
+{/if}
+{#if p}
+{#if domLoaded}
+<div transition:fly={{ duration:1000, delay:1000, x: "-100vw", opacity: 1, easing: cubicOut }} class="h-1 w-full lg:w-2/3 transition-width" 
+    style="background-image: repeating-linear-gradient(45deg, #E40303 0, #E40303 {thingWidth}em, #FF8C00 {thingWidth}em, #FF8C00 {thingWidth*2}em, 
+    #FFED00 {thingWidth*2}em, #FFED00 {thingWidth*3}em, #008026 {thingWidth*3}em, #008026 {thingWidth*4}em, #24408E {thingWidth*4}em, #24408E {thingWidth*5}em, 
+    #732982 {thingWidth*5}em, #732982 {thingWidth*6}em, white {thingWidth*6}em, white {thingWidth*7}em, #74D7EE {thingWidth*7}em, #74D7EE {thingWidth*8}em,
+    #FFAFC8 {thingWidth*8}em, #FFAFC8 {thingWidth*9}em, #613915 {thingWidth*9}em, #613915 {thingWidth*10}em, black {thingWidth*10}em, black {thingWidth*11}em)"></div>
 {/if}
 {/if}
