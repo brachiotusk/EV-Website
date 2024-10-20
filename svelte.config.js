795a52052f7a13008121fcb29b348967fc9c4ab2
Kireeti Kompella <127916417+brachiotusk@users.noreply.github.com>
2024-10-20 16:40:26 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: 'escape-velocity-14343/EV-Website'
+			base: '/escape-velocity-14343/EV-Website'
 		}
 	},
 	preprocess: vitePreprocess()
